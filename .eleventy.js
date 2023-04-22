6a35407c158ec672b6aa71ca7d88f8ebb0986aa3
ta <alt3753.7@gmail.com>
2023-04-22 21:27:48 +0700

    hmmmm

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,35 @@
 const markdownIt = require("markdown-it");
 const mdItFn = require('markdown-it-footnote');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   let options = {
     linkify: true,
     html: true
   };
 
-  eleventyConfig.setLibrary("md", markdownIt(options).use(mdItFn));
+  const md = markdownIt(options)
+
+  // Remember old renderer, if overridden, or proxy to default renderer
+  var defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    // If you are sure other plugins can't add `target` - drop check below
+    var aIndex = tokens[idx].attrIndex('rel');
+
+    // shitty eg
+    if (aIndex < 0) {
+      tokens[idx].attrPush(['rel', 'noreferrer']); // add new attribute
+    } else {
+      tokens[idx].attrs[aIndex][1] = 'noreferrer';    // replace value of existing attr
+    }
+
+    // pass token to default renderer.
+    return defaultRender(tokens, idx, options, env, self);
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
 
   eleventyConfig.addPassthroughCopy('assets')
 };
