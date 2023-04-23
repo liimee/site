7a8de5e91c6ad80383d42150a80e34bcfec473f1
ta <alt3753.7@gmail.com>
2023-04-23 17:10:42 +0700

    hmm

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ export const config = {
 };
 
 export default function middleware(request) {
-  if ((!request.headers.has('Referer') && request.headers.get('Sec-Fetch-Site') === 'cross-site') || request.headers.get('Referer') === 'https://electo.gq/') {
+  if ((!request.headers.has('Referer') && (request.headers.get('Sec-Fetch-Site') === 'cross-site' && request.headers.get('Sec-Fetch-User') === '?1')) || request.headers.get('Referer') === 'https://electo.gq/') {
     return Response.redirect(new URL('/needhelp', request.url));
   }
 
